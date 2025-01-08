d6cf4fd4ec671ca8b367dba1b77265e9fd567549
champaksworldcreate <champaksworld@gmail.com>
2025-01-08 15:02:47 +0530

    create a web server now using the node http package.

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Hello Node");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain',
+      
+    });
+    res.end('Hello from the node server');
+});
+
+server.listen(777, () => {
+    console.log('Server listening on port 777');
+});
